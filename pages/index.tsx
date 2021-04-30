52905e5f36387bb2f343ed07a4a78ec68f3e2e02
Abdul Salam <abdulsalam121196@gmail.com>
2021-05-01 01:08:08 +0700

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
